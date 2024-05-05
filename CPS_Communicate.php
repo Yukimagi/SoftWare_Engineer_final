afc557d97dcc25949acedb82d5958a054052d204
Yukimagi <yukilin55@gmail.com>
2024-05-05 17:26:31 +0800

    update CPS Template

---
+++ CPS_Communicate.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>CPS</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <style>
+        .vertical-line {
+          border-left: 1px solid #808080; /* 顏色與寬度等 */
+          height: 50px; /* 線的長度 */
+          margin: 0 20px; /* 位置 */
+        }
+        </style>
+    </head>
+    <body>
+        <?php
+        //連結資料庫
+        include("connection.php");
+        ?>
+        <?php
+session_start(); // 啟動 session
+
+// 檢查使用者是否已登入，如果未登入則重新導向到其他頁面
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+    $identity = "訪客";
+    $uid = "None";
+}
+else{
+// 從 session 中獲取使用者的身份和 ID
+$identity = $_SESSION['identity'];
+$uid = $_SESSION['uid'];
+}
+
+if (isset($identity) && $identity !== "SYS" && $identity !== "訪客") {
+
+    switch ($identity){
+        case "S":
+            $sql_query = "select name from basicinfo where uid='" . $uid . "'";
+            break;
+        case "T":
+            $sql_query = "select t_name as name from teacher_profile where t_uid='" . $uid . "'";
+            break;
+        case "L":
+            $sql_query = "select l_name as name from landlord where uid='" . $uid . "'";
+            break;
+    }
+    $result = mysql_query($sql_query);
+    $row = mysql_fetch_array($result);
+    $name = $row["name"];
+}
+?>
+        <!-- Responsive navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="CPS_Home.php">CPS</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="index02.php">Home</a></li>
+                        <!--<li class="nav-item"><a class="nav-link" href="#!">About</a></li>-->
+                        <!--<li class="nav-item"><a class="nav-link" href="#!">sign in</a></li>-->
+                        
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="CPS_Communicate.php">交流平台</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="CPS_OBJ.php">物件評價</a></li>
+                        <?php
+                        if(!($identity === "訪客")){
+                            echo'<li class="nav-item"><a class="nav-link active" aria-current="page" href="index01.php?logged_in=false">使用者登出</a></li>';
+                        }
+                        ?>
+                        <div class="vertical-line"></div><!-- 畫垂直線-->
+                        <p><span style="color:#b0c4de; display: inline;">現在身分為：</span>
+                    <?php
+                    if (isset($identity) && $identity === "SYS") {
+                        echo '<span style="color:#b0c4de; display: inline;">系統管理員</span>';
+                    }
+                    elseif (isset($identity) && $identity === "T") {
+                        echo '<span style="color:#b0c4de; display: inline;">教師</span>';
+                    }
+                    elseif (isset($identity) && $identity === "S") {
+                        echo '<span style="color:#b0c4de; display: inline;">學生</span>';
+                    }
+                    elseif (isset($identity) && $identity === "L") {
+                        echo '<span style="color:#b0c4de; display: inline;">房東</span>';
+                    }
+                    elseif(isset($identity) && $identity === "訪客") {
+                        echo '<span style="color:#b0c4de; display: inline;">訪客</span>';
+                    }
+
+                    if (isset($identity) && $identity !== "SYS"&& $identity !== "訪客") {
+                        echo '<br>';
+                        echo '<span style="color:#b0c4de; display: inline;">使用者姓名：</span><span style="color:#b0c4de; display: inline;">' . $name . '</span>';
+                    }
+                ?>
+            </p>
+            
+            
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Page header with logo and tagline-->
+        <header class="py-5 bg-light border-bottom mb-4">
+            <div class="container">
+                <div class="text-center my-5">
+                    <h1 class="fw-bolder">租屋交流平台</h1>
+                    <p class="lead mb-0">歡迎使用!</p>
+                </div>
+            </div>
+        </header>
+        <!-- Page content-->
+        <div class="container">
+            <div class="row">
+                <!-- Blog entries-->
+                <div class="col-lg-8">
+                    <!-- Featured blog post-->
+                    <div class="card mb-4">
+                        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg" alt="..." /></a>
+                        <div class="card-body">
+                            <div class="small text-muted">January 1, 2021</div>
+                            <h2 class="card-title">Featured Post Title</h2>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+                            <a class="btn btn-primary" href="#!">Read more →</a>
+                        </div>
+                    </div>
+                    <!-- Nested row for non-featured blog posts-->
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <!-- Blog post-->
+                            <div class="card mb-4">
+                                <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
+                                <div class="card-body">
+                                    <div class="small text-muted">January 1, 2021</div>
+                                    <h2 class="card-title h4">Post Title</h2>
+                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
+                                    <a class="btn btn-primary" href="#!">Read more →</a>
+                                </div>
+                            </div>
+                            <!-- Blog post-->
+                            <div class="card mb-4">
+                                <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
+                                <div class="card-body">
+                                    <div class="small text-muted">January 1, 2021</div>
+                                    <h2 class="card-title h4">Post Title</h2>
+                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
+                                    <a class="btn btn-primary" href="#!">Read more →</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <!-- Blog post-->
+                            <div class="card mb-4">
+                                <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
+                                <div class="card-body">
+                                    <div class="small text-muted">January 1, 2021</div>
+                                    <h2 class="card-title h4">Post Title</h2>
+                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
+                                    <a class="btn btn-primary" href="#!">Read more →</a>
+                                </div>
+                            </div>
+                            <!-- Blog post-->
+                            <div class="card mb-4">
+                                <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
+                                <div class="card-body">
+                                    <div class="small text-muted">January 1, 2021</div>
+                                    <h2 class="card-title h4">Post Title</h2>
+                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam.</p>
+                                    <a class="btn btn-primary" href="#!">Read more →</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Pagination-->
+                    <nav aria-label="Pagination">
+                        <hr class="my-0" />
+                        <ul class="pagination justify-content-center my-4">
+                            <li class="page-item disabled"><a class="page-link" href="#" tabindex="-1" aria-disabled="true">Newer</a></li>
+                            <li class="page-item active" aria-current="page"><a class="page-link" href="#!">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#!">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#!">3</a></li>
+                            <li class="page-item disabled"><a class="page-link" href="#!">...</a></li>
+                            <li class="page-item"><a class="page-link" href="#!">15</a></li>
+                            <li class="page-item"><a class="page-link" href="#!">Older</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <!-- Side widgets-->
+                <div class="col-lg-4">
+                    <!-- Search widget-->
+                    <div class="card mb-4">
+                        <div class="card-header">找好房</div>
+                        <div class="card-body">
+                            <div class="input-group">
+                                <input class="form-control" type="text" placeholder="Enter search term..." aria-label="Enter search term..." aria-describedby="button-search" />
+                                <button class="btn btn-primary" id="button-search" type="button">Go!</button>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Categories widget-->
+                    <div class="card mb-4">
+                        <div class="card-header">Categories</div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <ul class="list-unstyled mb-0">
+                                        <li><a href="#!">Web Design</a></li>
+                                        <li><a href="#!">HTML</a></li>
+                                        <li><a href="#!">Freebies</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-sm-6">
+                                    <ul class="list-unstyled mb-0">
+                                        <li><a href="#!">JavaScript</a></li>
+                                        <li><a href="#!">CSS</a></li>
+                                        <li><a href="#!">Tutorials</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Side widget-->
+                    <div class="card mb-4">
+                        <div class="card-header">Side Widget</div>
+                        <div class="card-body">You can put anything you want inside of these side widgets. They are easy to use, and feature the Bootstrap 5 card component!</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ index02.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>租屋管理系統</title>
+    <style>
+        /* 全局樣式 */
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f9f9f9;
+            overflow: hidden;
+        }
+
+        /* 頁眉樣式 */
+        header {
+            background-color: #333;
+            color: #fff;
+            padding: 20px 0;
+            text-align: center;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+
+        header h1 {
+            margin: 0;
+            font-size: 32px;
+        }
+
+        /* 導航欄樣式 */
+        nav {
+            float: left;
+            width: 200px;
+            background: #2c3e50;
+            color: #fff;
+            box-sizing: border-box;
+            height: 100vh;
+            overflow-y: auto;
+            transition: width 0.5s;
+            position: relative;
+        }
+
+        nav hr {
+            position: absolute;
+            bottom: 23vh;
+            width: 100%;
+        }
+
+        .LoginLink {
+            position: absolute;
+            bottom: 12vh;
+            padding-left: 40px;
+        }
+
+        /*
+        nav:hover {
+            width: 250px;
+        }
+        */
+
+        nav h2 {
+            margin-top: 0;
+            padding-left: 20px;
+        }
+
+        nav ul {
+            list-style-type: none;
+            padding: 0;
+            margin: 20px 0;
+        }
+
+        nav ul p {
+            color: #fff;
+            text-decoration: none;
+            transition: color 0.3s;
+            display: block;
+            padding-left: 10px;
+            position: absolute;
+            bottom: 15vh;
+        }
+
+        nav ul li {
+            padding-left: 20px;
+        }
+
+        nav ul li a {
+            color: lightskyblue;
+            text-decoration: none;
+            transition: color 0.3s;
+            display: block;
+            padding: 10px 0;
+        }
+
+        nav ul li a:hover {
+            color: #3498db;
+        }
+
+        /* 內容區域樣式 */
+        .content {
+            float: left;
+            width: calc(100% - 200px);
+            padding: 20px;
+            box-sizing: border-box;
+            transition: width 0.5s;
+        }
+
+        /* 响应式样式 */
+        @media only screen and (max-width: 768px) {
+            nav, .content {
+                width: 100%;
+            }
+
+            nav {
+                width: 100%;
+                height: auto;
+                position: relative;
+                margin-bottom: 20px;
+            }
+
+            nav:hover {
+                width: 100%;
+            }
+
+            .content {
+                width: 100%;
+            }
+        }
+    </style>
+</head>
+<body>
+    <?php
+    session_start(); // 啟動 session
+
+    // 檢查使用者是否已登入，如果未登入則重新導向到其他頁面
+    if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+        header('Location: index01.php'); // 重新導向到登入頁面
+        exit; // 終止程式執行
+    }
+    
+    // 從 session 中獲取使用者的身份和 ID
+    $identity = $_SESSION['identity'];
+    $uid = $_SESSION['uid'];
+
+    if (isset($identity) && $identity !== "SYS") {
+        //連接資料庫
+        include("connection.php");
+
+        switch ($identity){
+            case "S":
+                $sql_query = "select name from basicinfo where uid='" . $uid . "'";
+                break;
+            case "T":
+                $sql_query = "select t_name as name from teacher_profile where t_uid='" . $uid . "'";
+                break;
+            case "L":
+                $sql_query = "select l_name as name from landlord where uid='" . $uid . "'";
+                break;
+        }
+        $result = mysql_query($sql_query);
+        $row = mysql_fetch_array($result);
+        $name = $row["name"];
+    }
+    ?>
+
+
+    <header>
+        <h1>租屋管理系統</h1>
+    </header>
+    <nav>
+        <h2>功能選單</h2>
+        <ul>
+            <li><a href="#">首頁</a></li>
+            <?php
+                if (isset($identity) && $identity === "SYS") {
+                    echo '<li><a href="#">後台 - 帳號管理</a></li>';
+                }
+            ?>
+            <li><a href="#">租屋管理</a></li>
+            <li><a href="CPS_Home.php">交流平台</a></li>
+            <li><a href="#">廣告平台</a></li>
+            <?php
+                if (isset($identity) && $identity !== "SYS") {
+                    echo ' <li><a href="#">個人帳戶管理</a></li>';
+                }
+            ?>
+            <!-- 添加更多功能連結 -->
+            <hr> <!-- 添加分隔線 -->
+            <p>現在身分為：
+                <?php
+                    if (isset($identity) && $identity === "SYS") {
+                        echo '<span style="color:#b0c4de; display: inline;">系統管理員</span>';
+                    }
+                    elseif (isset($identity) && $identity === "T") {
+                        echo '<span style="color:#b0c4de; display: inline;">教師</span>';
+                    }
+                    elseif (isset($identity) && $identity === "S") {
+                        echo '<span style="color:#b0c4de; display: inline;">學生</span>';
+                    }
+                    elseif (isset($identity) && $identity === "L") {
+                        echo '<span style="color:#b0c4de; display: inline;">房東</span>';
+                    }
+
+
+                    if (isset($identity) && $identity !== "SYS") {
+                        echo '<br>';
+                        echo '使用者姓名：<span style="color:#b0c4de; display: inline;">' . $name . '</span>';
+                    }
+                ?>
+            </p>
+            <li class="LoginLink"><a href="index01.php?logged_in=false">使用者登出</a></li>
+        </ul>
+    </nav>
+    <div class="content">
+        <h2>歡迎來到租屋管理系統</h2>
+        <p>這裡是主要的內容區域，你可以在這裡添加租屋管理、交流平台和廣告平台的相關內容。</p>
+        <p>這個模板還可以進一步改進，比如添加用戶登錄、搜索功能、統計信息等等。你可以根據自己的需求進行調整和擴展。</p>
+    </div>
+</body>
+</html>
