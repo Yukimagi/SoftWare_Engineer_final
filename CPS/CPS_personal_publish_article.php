f29972e3d644f52dba46f7a5acc629b6ddfd2634
Yukimagi <yukilin55@gmail.com>
2024-05-06 22:45:05 +0800

    瀏覽:個人收藏(刪除)與發布文章(修改、刪除)

--- CPS/CPS_personal_publish_article.php
+++ CPS/CPS_personal_publish_article.php
@@ -111,7 +111,7 @@
         <header class="py-5 bg-light border-bottom mb-4">
             <div class="container">
                 <div class="text-center my-5">
-                    <h1 class="fw-bolder">文章交流</h1>
+                    <h1 class="fw-bolder">個人發布文章</h1>
                     
                 </div>
             </div>
@@ -136,40 +136,46 @@
                         }
                         else
                         {//查table
-                            $sql_query = "SELECT * FROM `contact article`";
+                            $sql_query = "SELECT * FROM `user_article` WHERE uid = '$uid'";
                             $result = mysql_query($sql_query);
 
                             // 查所有文章
                             while ($row = mysql_fetch_assoc($result)) {
                                 $articleID = $row['articleID'];
-                                $articleIname = $row['articleIname'];
-                                $articleIcontent = $row['articleIcontent'];
-                                $lovenum = $row['lovenum'];
-                                $keepnum = $row['keepnum'];
 
-                                // 輸出文章
-                                echo '<div class="card mb-4">';
-                                echo '<div class="card-body">';
-                                echo '<h2 class="card-title h4">' . $articleIname . '</h2>';
-                                echo '<p class="card-text">' . $articleIcontent . '</p>';
-                                echo'<ul class="list-unstyled mb-0">';
-                                echo '<li><span>Likes: ' . $lovenum . '</span>';
-                                //echo '<a class="btn btn-primary btn-sm custom-btn" style="margin-left: 19px;" href="CPS_dataProcess/update_love.php?articleID=' . $articleID . '">按讚</a></li>';
-                                if (!($identity === "SYS"||$identity === "L"|| $identity === "訪客")) {
-                                echo '<button class="btn btn-primary btn-sm custom-btn"style="margin-left: 19px;" onclick="loveArticle(\'' . $articleID . '\')">按讚</button></li>';
+                                $sql_query2 = "SELECT * FROM `contact article` WHERE articleID = '$articleID'";
+                                $result2 = mysql_query($sql_query2);
+                                while ($row2 = mysql_fetch_assoc($result2)) {
+                                    $articleIname = $row2['articleIname'];
+                                    $articleIcontent = $row2['articleIcontent'];
+                                    $lovenum = $row2['lovenum'];
+                                    $keepnum = $row2['keepnum'];
+
+                                    // 輸出文章
+                                    echo '<div class="card mb-4">';
+                                    echo '<div class="card-body">';
+                                    echo '<h2 class="card-title h4">' . $articleIname . '</h2>';
+                                    echo '<p class="card-text">' . $articleIcontent . '</p>';
+                                    echo'<ul class="list-unstyled mb-0">';
+                                    echo '<li><span>Likes: ' . $lovenum . '</span>';
+                                    //echo '<a class="btn btn-primary btn-sm custom-btn" style="margin-left: 19px;" href="CPS_dataProcess/update_love.php?articleID=' . $articleID . '">按讚</a></li>';
+                                    if (!($identity === "SYS"||$identity === "L"|| $identity === "訪客")) {
+                                    //echo '<button class="btn btn-primary btn-sm custom-btn"style="margin-left: 19px;" onclick="loveArticle(\'' . $articleID . '\')">按讚</button></li>';
+                                    }
+                                    //echo '<span>Likes: ' . $lovenum . '</span>';
+                                    //echo '';
+                                    echo '<li><span>Keeps: ' . $keepnum . '</span>';
+                                    if (!($identity === "SYS"||$identity === "L"|| $identity === "訪客")) {
+                                    //echo '<button class="btn btn-primary btn-sm custom-btn" style="margin-left: 10px;" onclick="keepArticle(\'' . $uid . '\', \'' . $articleID . '\')">收藏</button></li>';
+                                    }
+                                    echo'</ul>';
+                                    echo'<ul class="list-unstyled mb-0">';
+                                    echo'<li><a class="btn btn-primary btn-sm custom-btn" href="CPS_Artical_Modify.php?articleID=' . $articleID . '">修改文章</a>';
+                                    echo '<button class="btn btn-primary btn-sm custom-btn" style="margin-left: 10px;" onclick="DeleteArticle(\'' . $uid . '\', \'' . $articleID . '\')">刪除</button></li>';
+                                    echo'</ul>';
+                                    echo '</div>';
+                                    echo '</div>';
                                 }
-                                //echo '<span>Likes: ' . $lovenum . '</span>';
-                                //echo '';
-                                echo '<li><span>Keeps: ' . $keepnum . '</span>';
-                                if (!($identity === "SYS"||$identity === "L"|| $identity === "訪客")) {
-                                echo '<button class="btn btn-primary btn-sm custom-btn" style="margin-left: 10px;" onclick="keepArticle(\'' . $uid . '\', \'' . $articleID . '\')">收藏</button></li>';
-                                }
-                                echo'</ul>';
-                                echo'<ul class="list-unstyled mb-0">';
-                                echo'<li><a class="btn btn-primary btn-sm custom-btn" href="CPS_Artical_Response.php?articleID=' . $articleID . '">Read more →</a></li>';
-                                echo'</ul>';
-                                echo '</div>';
-                                echo '</div>';
                             }
                         }
                     ?>
@@ -177,24 +183,9 @@
                     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
                     <script>
                     // JavaScript 
-                    function loveArticle(articleID) {
-                        
+                    function DeleteArticle(uid,articleID) {
                         $.ajax({
-                            url: 'CPS_dataProcess/update_love.php', 
-                            type: 'POST',
-                            data: { articleID: articleID }, 
-                            success: function(response) {
-                                // 重新加載頁面
-                                location.reload();
-                            }
-                        });
-                    }
-
-                    
-                    function keepArticle(uid, articleID) {
-                        
-                        $.ajax({
-                            url: 'CPS_dataProcess/update_keep.php', 
+                            url: 'CPS_dataProcess/delete_article.php', 
                             type: 'POST',
                             data: { uid: uid, articleID: articleID },
                             success: function(response) {
@@ -203,47 +194,73 @@
                             }
                         });
                     }
-
                     </script>
+                    
                 </div>
                 <!-- Side widgets-->
                 <div class="col-lg-4">
-                    <!-- Search widget-->
-                    <div class="card mb-4">
-                    <div class="card-header">找文章</div>
-                    <div class="card-body">
-                            <div class="input-group">
-                                <input class="form-control" type="text" placeholder="Enter search term..." aria-label="Enter search term..." aria-describedby="button-search" />
-                                <button class="btn btn-primary" id="button-search" type="button">Go!</button>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Categories widget-->
-                    <div class="card mb-4">
-                        <div class="card-header">Categories</div>
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <ul class="list-unstyled mb-0">
-                                        <li><a href="#!">Web Design</a></li>
-                                        <li><a href="#!">HTML</a></li>
-                                        <li><a href="#!">Freebies</a></li>
-                                    </ul>
-                                </div>
-                                <div class="col-sm-6">
-                                    <ul class="list-unstyled mb-0">
-                                        <li><a href="#!">JavaScript</a></li>
-                                        <li><a href="#!">CSS</a></li>
-                                        <li><a href="#!">Tutorials</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    
                     <!-- Side widget-->
                     <div class="card mb-4">
-                        <div class="card-header">Side Widget</div>
-                        <div class="card-body">You can put anything you want inside of these side widgets. They are easy to use, and feature the Bootstrap 5 card component!</div>
+                        <div class="card-header">personal profile</div>
+                        <?php
+                            if (!empty($identity) && !empty($uid)) {
+                                if ($identity === "T") {
+                                    // 查询教师信息表
+                                    $table = "teacher_profile";
+                                    $columns = "t_uid, t_name, t_rank, t_tel, t_mail, t_officetel";
+                                } elseif ($identity === "S") {
+                                    // 查询学生基本信息表
+                                    $table = "basicinfo";
+                                    $columns = "uid, SID, name, grade, gender, phone, email";
+                                }
+                            
+                                // 构建 SQL 查询语句
+                                $sql_query = "SELECT $columns FROM `$table` WHERE t_uid = '$uid'";
+                                if ($identity === "S") {
+                                    $sql_query = "SELECT $columns FROM `$table` WHERE uid = '$uid'";
+                                }
+                            
+                                $result = mysql_query($sql_query);
+                            
+                                if ($result) {
+                                    // 输出查询结果
+                                    while ($row = mysql_fetch_assoc($result)) {
+                                        // 输出指定的列
+                                        echo '<div class="card-body">';
+                                        foreach ($row as $key => $value) {
+                                            if ($key === "t_uid" || $key === "uid") {
+                                                $key_text = "UID";
+                                            } else if ($key === "t_name" || $key === "name") {
+                                                $key_text = "姓名";
+                                            } else if ($key === "t_rank") {
+                                                $key_text = "職等";
+                                            } else if ($key === "t_tel" || $key === "phone") {
+                                                $key_text = "電話";
+                                            } else if ($key === "t_mail" || $key === "email") {
+                                                $key_text = "信箱";
+                                            } else if ($key === "t_officetel") {
+                                                $key_text = "辦公室電話";
+                                            } else if ($key === "SID") {
+                                                $key_text = "學號";
+                                            } else if ($key === "grade") {
+                                                $key_text = "年級";
+                                            } else if ($key === "gender") {
+                                                $key_text = "性別";
+                                            }
+                                            echo "$key_text: $value <br>";
+                                        }
+                                        echo "</div>";
+                                    }
+                                } else {
+                                    echo "查询失败：" . mysql_error();
+                                }
+                            } else {
+                                echo "未提供足够的信息进行查询。";
+                            }
+                            
+                        ?>
+                        
                     </div>
                 </div>
             </div>
