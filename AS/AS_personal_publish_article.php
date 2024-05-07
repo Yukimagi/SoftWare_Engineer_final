588aa54b6d0778bb5ad39fc25a244ffd2a4c7d50
sherrylittlefish <yuyung9201@gmail.com>
2024-05-07 21:56:43 +0800

    AS_v1.0(廣告)

---
+++ AS/AS_personal_publish_article.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>CPS</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <style>
+        .vertical-line {
+          border-left: 1px solid #808080; /* 顏色與寬度等 */
+          height: 50px; /* 線的長度 */
+          margin: 0 20px; /* 位置 */
+        }
+        </style>
+    </head>
+    <body>
+        <?php
+        //連結資料庫
+        include("../connection.php");
+        ?>
+        <?php
+            session_start(); // 啟動 session
+
+            // 檢查使用者是否已登入，如果未登入則重新導向到其他頁面
+            if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+                $identity = "訪客";
+                $uid = "None";
+            }
+            else{
+            // 從 session 中獲取使用者的身份和 ID
+            $identity = $_SESSION['identity'];
+            $uid = $_SESSION['uid'];
+            }
+
+            if (isset($identity) && $identity !== "SYS" && $identity !== "訪客") {
+
+                switch ($identity){
+                    case "S":
+                        $sql_query = "select name from basicinfo where uid='" . $uid . "'";
+                        break;
+                    case "T":
+                        $sql_query = "select t_name as name from teacher_profile where t_uid='" . $uid . "'";
+                        break;
+                    case "L":
+                        $sql_query = "select l_name as name from landlord where uid='" . $uid . "'";
+                        break;
+                }
+                $result = mysql_query($sql_query);
+                $row = mysql_fetch_array($result);
+                $name = $row["name"];
+            }
+            ?>
+        <!-- Responsive navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="CPS_Home.php">CPS</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="../index02.php">Home</a></li>
+                        <!--<li class="nav-item"><a class="nav-link" href="#!">About</a></li>-->
+                        <!--<li class="nav-item"><a class="nav-link" href="#!">sign in</a></li>-->
+                        <?php
+                        if (!($identity === "SYS"||$identity === "L"|| $identity === "訪客")) {
+                        echo '<li class="nav-item"><a class="nav-link active" aria-current="page" href="CPS_Publish_Artical.php">刊登文章</a></li>';
+                        }
+                        ?>
+                        <!--<li class="nav-item"><a class="nav-link active" aria-current="page" href="CPS_OBJ.php">物件評價</a></li>-->
+                        <?php
+                        if(!($identity === "訪客")){
+                            echo'<li class="nav-item"><a class="nav-link active" aria-current="page" href="../index01.php?logged_in=false">使用者登出</a></li>';
+                        }
+                        ?>
+                        <div class="vertical-line"></div><!-- 畫垂直線-->
+                        <p><span style="color:#b0c4de; display: inline;">現在身分為：</span>
+                    <?php
+                    if (isset($identity) && $identity === "SYS") {
+                        echo '<span style="color:#b0c4de; display: inline;">系統管理員</span>';
+                    }
+                    elseif (isset($identity) && $identity === "T") {
+                        echo '<span style="color:#b0c4de; display: inline;">教師</span>';
+                    }
+                    elseif (isset($identity) && $identity === "S") {
+                        echo '<span style="color:#b0c4de; display: inline;">學生</span>';
+                    }
+                    elseif (isset($identity) && $identity === "L") {
+                        echo '<span style="color:#b0c4de; display: inline;">房東</span>';
+                    }
+                    elseif(isset($identity) && $identity === "訪客") {
+                        echo '<span style="color:#b0c4de; display: inline;">訪客</span>';
+                    }
+
+                    if (isset($identity) && $identity !== "SYS"&& $identity !== "訪客") {
+                        echo '<br>';
+                        echo '<span style="color:#b0c4de; display: inline;">使用者姓名：</span><span style="color:#b0c4de; display: inline;">' . $name . '</span>';
+                    }
+                ?>
+            </p>
+            
+            
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Page header with logo and tagline-->
+        <header class="py-5 bg-light border-bottom mb-4">
+            <div class="container">
+                <div class="text-center my-5">
+                    <h1 class="fw-bolder">個人發布文章</h1>
+                    
+                </div>
+            </div>
+        </header>
+        <!-- Page content-->
+        <div class="container">
+            <div class="row">
+                <!-- Blog entries-->
+                <div class="col-lg-8">
+                    <!-- Featured blog post-->
+                    <div class="card mb-4">
+                        <a href="#!"><img class="card-img-top" src="assets/article_warn.png" alt="..." /></a>
+                        <div class="card-body">
+
+                        </div>
+                    </div>
+                    <?php
+                        $select_db=@mysql_select_db("rentsystem");//選擇資料庫
+                        if(!$select_db)
+                        {
+                        echo'<br>找不到資料庫!<br>';
+                        }
+                        else
+                        {//查table
+                            $sql_query = "SELECT * FROM `user_article` WHERE uid = '$uid'";
+                            $result = mysql_query($sql_query);
+
+                            // 查所有文章
+                            while ($row = mysql_fetch_assoc($result)) {
+                                $articleID = $row['articleID'];
+
+                                $sql_query2 = "SELECT * FROM `contact article` WHERE articleID = '$articleID'";
+                                $result2 = mysql_query($sql_query2);
+                                while ($row2 = mysql_fetch_assoc($result2)) {
+                                    $articleIname = $row2['articleIname'];
+                                    $articleIcontent = $row2['articleIcontent'];
+                                    $lovenum = $row2['lovenum'];
+                                    $keepnum = $row2['keepnum'];
+
+                                    // 輸出文章
+                                    echo '<div class="card mb-4">';
+                                    echo '<div class="card-body">';
+                                    echo '<h2 class="card-title h4">' . $articleIname . '</h2>';
+                                    echo '<p class="card-text">' . $articleIcontent . '</p>';
+                                    echo'<ul class="list-unstyled mb-0">';
+                                    echo '<li><span>Likes: ' . $lovenum . '</span>';
+                                    //echo '<a class="btn btn-primary btn-sm custom-btn" style="margin-left: 19px;" href="CPS_dataProcess/update_love.php?articleID=' . $articleID . '">按讚</a></li>';
+                                    if (!($identity === "SYS"||$identity === "L"|| $identity === "訪客")) {
+                                    //echo '<button class="btn btn-primary btn-sm custom-btn"style="margin-left: 19px;" onclick="loveArticle(\'' . $articleID . '\')">按讚</button></li>';
+                                    }
+                                    //echo '<span>Likes: ' . $lovenum . '</span>';
+                                    //echo '';
+                                    echo '<li><span>Keeps: ' . $keepnum . '</span>';
+                                    if (!($identity === "SYS"||$identity === "L"|| $identity === "訪客")) {
+                                    //echo '<button class="btn btn-primary btn-sm custom-btn" style="margin-left: 10px;" onclick="keepArticle(\'' . $uid . '\', \'' . $articleID . '\')">收藏</button></li>';
+                                    }
+                                    echo'</ul>';
+                                    echo'<ul class="list-unstyled mb-0">';
+                                    echo'<li><a class="btn btn-primary btn-sm custom-btn" href="CPS_Artical_Modify.php?articleID=' . $articleID . '">修改文章</a>';
+                                    echo '<button class="btn btn-primary btn-sm custom-btn" style="margin-left: 10px;" onclick="DeleteArticle(\'' . $uid . '\', \'' . $articleID . '\')">刪除</button></li>';
+                                    echo'</ul>';
+                                    echo '</div>';
+                                    echo '</div>';
+                                }
+                            }
+                        }
+                    ?>
+                    <!-- 記得引入函數-->
+                    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+                    <script>
+                    // JavaScript 
+                    function DeleteArticle(uid,articleID) {
+                        $.ajax({
+                            url: 'CPS_dataProcess/delete_article.php', 
+                            type: 'POST',
+                            data: { uid: uid, articleID: articleID },
+                            success: function(response) {
+                                // 重新加載頁面
+                                location.reload();      
+                            }
+                        });
+                    }
+                    </script>
+                    
+                </div>
+                <!-- Side widgets-->
+                <div class="col-lg-4">
+                    
+                    <!-- Side widget-->
+                    <div class="card mb-4">
+                        <div class="card-header">personal profile</div>
+                        <?php
+                            if (!empty($identity) && !empty($uid)) {
+                                if ($identity === "T") {
+                                    // 查询教师信息表
+                                    $table = "teacher_profile";
+                                    $columns = "t_uid, t_name, t_rank, t_tel, t_mail, t_officetel";
+                                } elseif ($identity === "S") {
+                                    // 查询学生基本信息表
+                                    $table = "basicinfo";
+                                    $columns = "uid, SID, name, grade, gender, phone, email";
+                                }
+                            
+                                // 构建 SQL 查询语句
+                                $sql_query = "SELECT $columns FROM `$table` WHERE t_uid = '$uid'";
+                                if ($identity === "S") {
+                                    $sql_query = "SELECT $columns FROM `$table` WHERE uid = '$uid'";
+                                }
+                            
+                                $result = mysql_query($sql_query);
+                            
+                                if ($result) {
+                                    // 输出查询结果
+                                    while ($row = mysql_fetch_assoc($result)) {
+                                        // 输出指定的列
+                                        echo '<div class="card-body">';
+                                        foreach ($row as $key => $value) {
+                                            if ($key === "t_uid" || $key === "uid") {
+                                                $key_text = "UID";
+                                            } else if ($key === "t_name" || $key === "name") {
+                                                $key_text = "姓名";
+                                            } else if ($key === "t_rank") {
+                                                $key_text = "職等";
+                                            } else if ($key === "t_tel" || $key === "phone") {
+                                                $key_text = "電話";
+                                            } else if ($key === "t_mail" || $key === "email") {
+                                                $key_text = "信箱";
+                                            } else if ($key === "t_officetel") {
+                                                $key_text = "辦公室電話";
+                                            } else if ($key === "SID") {
+                                                $key_text = "學號";
+                                            } else if ($key === "grade") {
+                                                $key_text = "年級";
+                                            } else if ($key === "gender") {
+                                                $key_text = "性別";
+                                            }
+                                            echo "$key_text: $value <br>";
+                                        }
+                                        echo "</div>";
+                                    }
+                                } else {
+                                    echo "查询失败：" . mysql_error();
+                                }
+                            } else {
+                                echo "未提供足够的信息进行查询。";
+                            }
+                            
+                        ?>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Rent Management System 2024</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ index01.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>租屋管理系統</title>
+    <style>
+        /* 全局樣式 */
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f9f9f9;
+            overflow: hidden;
+        }
+
+        /* 頁眉樣式 */
+        header {
+            background-color: #333;
+            color: #fff;
+            padding: 20px 0;
+            text-align: center;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+
+        header h1 {
+            margin: 0;
+            font-size: 32px;
+        }
+
+        /* 導航欄樣式 */
+        nav {
+            float: left;
+            width: 200px;
+            background: #2c3e50;
+            color: #fff;
+            box-sizing: border-box;
+            height: 100vh;
+            overflow-y: auto;
+            transition: width 0.5s;
+            position: relative;
+        }
+
+        nav hr {
+            position: absolute;
+            bottom: 23vh;
+            width: 100%;
+        }
+
+        .LoginLink {
+            position: absolute;
+            bottom: 14vh;
+            padding-left: 20px;
+        }
+
+        .ApplyLink {
+            position: absolute;
+            bottom: 10vh;
+            padding-left: 20px;
+        }
+
+        /*
+        nav:hover {
+            width: 250px;
+        }
+        */
+
+        nav h2 {
+            margin-top: 0;
+            padding-left: 20px;
+        }
+
+        nav ul {
+            list-style-type: none;
+            padding: 0;
+            margin: 20px 0;
+        }
+
+        nav ul p {
+            color: #fff;
+            text-decoration: none;
+            transition: color 0.3s;
+            display: block;
+            padding-left: 20px;
+            position: absolute;
+            bottom: 17vh;
+        }
+
+        nav ul li {
+            padding-left: 20px;
+        }
+
+        nav ul li a {
+            color: lightskyblue;
+            text-decoration: none;
+            transition: color 0.3s;
+            display: block;
+            padding: 10px 0;
+        }
+
+        nav ul li a:hover {
+            color: #3498db;
+        }
+
+        /* 內容區域樣式 */
+        .content {
+            float: left;
+            width: calc(100% - 200px);
+            padding: 20px;
+            box-sizing: border-box;
+            transition: width 0.5s;
+        }
+
+        /* 响应式样式 */
+        @media only screen and (max-width: 768px) {
+            nav, .content {
+                width: 100%;
+            }
+
+            nav {
+                width: 100%;
+                height: auto;
+                position: relative;
+                margin-bottom: 20px;
+            }
+
+            nav:hover {
+                width: 100%;
+            }
+
+            .content {
+                width: 100%;
+            }
+        }
+    </style>
+</head>
+<body>
+    <?php
+        session_start(); // 啟動 session
+
+        if (isset($_GET['logged_in']) && $_GET['logged_in'] === 'false') {
+            // 清除所有的 session 資料
+            session_unset();
+            // 銷毀 session
+            session_destroy();
+            // 重新導向到登入頁面或其他適合的頁面
+            header('Location: index01.php');
+            exit;
+        }
+    ?>
+    <header>
+        <h1>租屋管理系統</h1>
+    </header>
+    <nav>
+        <h2>功能選單</h2>
+        <ul>
+            <li><a href="index01.php">首頁</a></li>
+            <li><a href="AS/AS_Home.php">廣告平台</a></li>
+            <li><a href="CPS/CPS_Home.php">交流平台</a></li>
+            <!-- 添加更多功能連結 -->
+            <hr> <!-- 添加分隔線 -->
+            <p>現在身分為：<span style="color:#b0c4de; display: inline;">訪客</span></p>
+            <li class="LoginLink"><a href="loginIndex.php">使用者登入</a></li>
+            <li class="ApplyLink"><a href="">帳號申請</a></li>
+        </ul>
+    </nav>
+    <div class="content">
+        <h2>歡迎來到租屋管理系統</h2>
+        <p>這裡是主要的內容區域，你可以在這裡添加租屋管理、交流平台和廣告平台的相關內容。</p>
+        <p>這個模板還可以進一步改進，比如添加用戶登錄、搜索功能、統計信息等等。你可以根據自己的需求進行調整和擴展。</p>
+    </div>
+</body>
+</html>
