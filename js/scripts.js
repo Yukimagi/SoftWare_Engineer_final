28ddff70b9bfd254c4e93fc56009555c22712f00
javen <javen1530@gmail.com>
2024-05-11 14:06:17 +0800

    Integrate necessary functions.

--- js/scripts.js
+++ js/scripts.js
@@ -21,25 +21,70 @@ function warning3(){
     window.location.href='SAS.php';
 }
 function warning4(){
-    var message = '發生內部錯誤4';
+    var message = '發生內部錯4誤';
     alert(message);
     window.location.href='SAS.php';
+}
+function warning5(){
+    var message = '發生內部錯誤5';
+    window.location.href='SAS_UserDelete.php';
+}
+function warning6(){
+    var message = '發生內部錯誤6';
+    window.location.href='SAS_UserDelete.php';
 }
 function logoutmsg(){
     var message = '已從系統登出';
     alert(message);
     window.location.href="index.php";
 }
-function updatemsg(){
+function updatemsgForSAS(){
     var message = '已更新資料';
     alert(message);
     window.location.href="SAS.php";
 }
+function updatemsg(){
+    var message = '已更新資料';
+    alert(message);
+    window.location.href="lobby.php";
+}
+function deletemsg(){
+    var message = '已刪除資料';
+    alert(message);
+    window.location.href="SAS_UserDelete.php";
+}
+function createmsg(){
+    var message = '已新增資料';
+    alert(message);
+    window.location.href="SAS.php";
+}
+function signmissing(){
+    var message = '驗證遺失';
+    alert(message);
+    window.location.href="lobby.php";
+}
 ////////////////////////////////////////
 function showiframe1(){
     var iframe = document.getElementById('user_profile');
+    var disableiframe1 = document.getElementById('user_delete');
     iframe.style.display = 'block';
+    disableiframe1.style.display = 'none';
 }
+function showiframe2(){{
+    var iframe = document.getElementById('user_delete');
+    var disableiframe1 = document.getElementById('user_profile');
+    iframe.style.display = 'block';
+    disableiframe1.style.display = 'none';
+}}
+////////////////////////////////////////
 function backtoSAS(){
     window.location.href='SAS.php';
+}
+////////////////////////////////////////
+function deleteconfirm(){
+    var confirmation = confirm('是否刪除該使用者帳號?');
+    if(confirmation){
+    }else{
+        alert('已取消操作');
+    }
 }
