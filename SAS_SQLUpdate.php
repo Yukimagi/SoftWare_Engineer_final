e771f4ffb3e7c44c690a5645a7dce538d8c5354e
javen <javen1530@gmail.com>
2024-05-11 15:42:36 +0800

    data update 05/11.

--- SAS_SQLUpdate.php
+++ SAS_SQLUpdate.php
@@ -17,13 +17,21 @@ if (isset($_POST['uid'])) {
             $phone = $_POST['office_phone'];
 
             $sql_query = "UPDATE teacher_profile SET 
-                teacher_profile.t_name = '$name',
-                teacher_profile.t_rank = '$rank',
-                teacher_profile.t_tel = '$tel',
-                teacher_profile.t_mail = '$mail',
-                teacher_profile.t_address = '$location',
-                teacher_profile.t_officetel = '$phone'
-                WHERE teacher_profile.t_uid = '$user_id'";
+                teacher_profile.t_name = :t_name,
+                teacher_profile.t_rank = :t_rank,
+                teacher_profile.t_tel = :t_tel,
+                teacher_profile.t_mail = :t_mail,
+                teacher_profile.t_address = :t_address,
+                teacher_profile.t_officetel = :t_officetel
+                WHERE teacher_profile.t_uid = :t_uid";
+            $result = $conn->prepare($sql_query);
+            $result->bindParam(":t_name", $name);
+            $result->bindParam(":t_rank", $rank);
+            $result->bindParam(":t_tel", $tel);
+            $result->bindParam(":t_mail", $mail);
+            $result->bindParam(":t_address", $location);
+            $result->bindParam(":t_officetel", $phone);
+            $result->bindParam(":t_uid", $user_id);
             break;
         case 'S':
             $name = $_POST['student_name'];
@@ -39,17 +47,29 @@ if (isset($_POST['uid'])) {
             $contphone = $_POST['student_contphone'];
 
             $sql_query = "UPDATE basicinfo SET 
-                basicinfo.SID = '$id',
-                basicinfo.name = '$name',
-                basicinfo.grade = '$grade',
-                basicinfo.gender = '$gender',
-                basicinfo.phone = '$phone',
-                basicinfo.email = '$email',
-                basicinfo.HomeAddr = '$haddr',
-                basicinfo.Hphone = '$htel',
-                basicinfo.Contactor = '$cont',
-                basicinfo.Cphone = '$contphone'
-                WHERE basicinfo.uid = '$user_id'";
+                basicinfo.SID = :SID,
+                basicinfo.name = :name,
+                basicinfo.grade = :grade,
+                basicinfo.gender = :gender,
+                basicinfo.phone = :phone,
+                basicinfo.email = :email,
+                basicinfo.HomeAddr = :HomeAddr,
+                basicinfo.Hphone = :Hphone,
+                basicinfo.Contactor = :Contactor,
+                basicinfo.Cphone = :Cphone
+                WHERE basicinfo.uid = :uid";
+            $result = $conn->prepare($sql_query);
+            $result->bindParam(":SID", $id);
+            $result->bindParam(":name", $name);
+            $result->bindParam(":grade", $grade);
+            $result->bindParam(":gender", $gender);
+            $result->bindParam(":phone", $phone);
+            $result->bindParam(":email", $email);
+            $result->bindParam(":HomeAddr", $haddr);
+            $result->bindParam(":Hphone", $htel);
+            $result->bindParam(":Contactor", $cont);
+            $result->bindParam(":Cphone", $contphone);
+            $result->bindParam(":user_id", $user_id);
             break;
         case 'L':
             $name = $_POST['landlord_name'];
@@ -58,24 +78,27 @@ if (isset($_POST['uid'])) {
             $line = $_POST['landlord_line'];
 
             $sql_query = "UPDATE landlord SET 
-                landlord.l_name = '$name',
-                landlord.l_gender = '$gender',
-                landlord.l_phone = '$phone',
-                landlord.l_line = '$line'
-                WHERE landlord.uid = '$user_id'";
+                landlord.l_name = :l_name,
+                landlord.l_gender = :l_gender,
+                landlord.l_phone = :l_phone,
+                landlord.l_line = :l_line
+                WHERE landlord.uid = :uid";
+            $result = $conn->prepare($sql_query);
+            $result->bindParam(":l_name", $name);
+            $result->bindParam(":l_gender", $gender);
+            $result->bindParam(":l_phone", $phone);
+            $result->bindParam(":l_line", $line);
+            $result->bindParam(":uid", $user_id);
             break;
     }
-    $result = $conn->query($sql_query);
-    if ($result === false) {
-        // SQL語句執行失敗
-        echo '<script>warning3()</script>';
-    } else {
-        // SQL語句執行成功
-        if ($result->execute()) {
+    if ($result->execute()) {
+        if (isset($_POST['NotSAS']) && $_POST['NotSAS'] === "true"){
             echo '<script>updatemsg()</script>';
-        } else {
-            echo '<script>warning4()</script>';
+        }else{
+            echo '<script>updatemsgForSAS()</script>';
         }
+    } else {
+        echo '<script>warning4()</script>';
     }
     exit;
 }
