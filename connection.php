93fb8359304968416dde16d5e31a40aab4471080
danny4270 <chses10130213@gmail.com>
2024-05-11 14:08:03 +0800

    connection

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+$DB_severname = 'localhost';
+$DB_username = 'root';
+$DB_password = '32438654';
+$DB_database = 'rentsystem';
+try{
+    $conn = new PDO("mysql:host=$DB_severname;dbname=$DB_database", $DB_username, $DB_password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch (PDOException $e){
+    echo "資料庫連接失敗" . $e->getMessage();
+}
