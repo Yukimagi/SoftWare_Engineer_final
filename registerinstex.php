dba87cb00d68669b0a5e30735636e942efe9c916
javen <javen1530@gmail.com>
2024-05-24 14:36:51 +0800

    Feat: 註冊頁面

--- registerinstex.php
+++ registerinstex.php
@@ -1,4 +1,14 @@
 <?php
+include("SAS/connection.php");
+
+$sql_query = "SELECT MAX(account_applications.id) as now FROM account_applications";
+$result = $conn->query($sql_query);
+$row = $result->fetch(PDO::FETCH_ASSOC);
+$currentid = $row['now'];
+$numberPart = (int)substr($currentid, 2); // 提取數字部分，並轉換為整數
+$newNumberPart = $numberPart + 1; // 增加 1
+$new_id = "ap" . str_pad($newNumberPart, 5, "0", STR_PAD_LEFT); // 補齊零填充並連接回 "ap" 字母
+
 ?>
 <!DOCTYPE>
 <html lang="zh-Hant">
@@ -7,6 +17,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title>會員註冊</title>
     <link href="css/registerindex.css" rel="stylesheet" />
+    <script src="js/scripts.js"></script>
 </head>
 <body>
 <header>
@@ -26,27 +37,35 @@
             <div id="title">
                 <h1>註冊</h1>
             </div>
-            <form action="registerprocess.php" method="post">
+            <form action="applyprocess.php" method="post">
                 <div class="profile_input">
-                    <input type="text" required>
+                    <input type="text" name="name" required>
                     <span>使用者姓名</span>
                     <i></i>
                 </div>
                 <div class="profile_input">
-                    <input type="text" required>
+                    <input type="text" name="phone" required>
                     <span>使用者電話</span>
                     <i></i>
                 </div>
                 <div class="profile_input">
                     <input type="text" required>
+                    <span>電子郵件</span>
+                    <i></i>
+                </div>
+                <div class="profile_input">
+                    <input type="text" name="reason" required>
                     <span>個人簡介</span>
                     <i></i>
                 </div>
+                <?php
+                echo '<input type="hidden" name="id" value="' . $new_id . '">';
+                ?>
                 <div id="submit_button">
-                    <input type="submit" value="提交">
+                    <input type="submit" value="提交" class="register_button" id="top_button">
                 </div>
                 <div id="backtoindex">
-                    <input type="button" value="返回">
+                    <input type="button" value="返回" class="register_button" id="bottom_button" onclick="backtoindex()">
                 </div>
             </form>
         </div>
