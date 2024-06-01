e6483e6e2925776a6bc3a66b95be9db7ee48d85d
izxizx <tiffany920124@gmail.com>
2024-06-01 12:02:51 +0800

    個人資料IS連到對應頁面

--- IS/IS_personal_information.php
+++ IS/IS_personal_information.php
@@ -65,7 +65,8 @@
         <!-- Responsive navbar-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
-                <a class="navbar-brand" href="IS_Home.php">IS</a>
+                <?php if ($identity === "T") echo "<a class=\"navbar-brand\" href=\"IS_teacher_records.php\">IS</a>"; ?>
+                <?php if ($identity === "S") echo "<a class=\"navbar-brand\" href=\"IS_Home.php\">IS</a>"; ?>               
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
