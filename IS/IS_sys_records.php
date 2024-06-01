ed875069bb37933a4c5fb72e58a1a5b647161f34
sherrylittlefish <yuyung9201@gmail.com>
2024-06-01 18:48:31 +0800

    管理員可以查看那些學生已填/未填訪談紀錄表(點IS)

--- IS/IS_sys_records.php
+++ IS/IS_sys_records.php
@@ -58,7 +58,7 @@
         <!-- 上面的標籤-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
-                <a class="navbar-brand" href="IS_sys_records.php">IS</a>
+                <a class="navbar-brand" href="IS_sys_search_records.php">IS</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
