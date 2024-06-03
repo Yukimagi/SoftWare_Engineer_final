80ec338f76642d94d80973bec924c36ca12994be
izxizx <143394124+izxizx@users.noreply.github.com>
2024-06-03 20:46:48 +0800

    Update AS_AD_all_landlords_ad_modify.php

--- AS/AS_AD_all_landlords_ad_modify.php
+++ AS/AS_AD_all_landlords_ad_modify.php
@@ -64,7 +64,7 @@
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
 
 
-                        <li class="nav-item"><a class="nav-link" href="../lobby.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../index.php">Home</a></li>
                         <!--<li class="nav-item"><a class="nav-link" href="#!">About</a></li>-->
                         <!--<li class="nav-item"><a class="nav-link" href="#!">sign in</a></li>-->
                        
