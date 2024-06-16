923bcf44f5674936a6ee99f1451ffc0e27ec8af2
danny4270 <148946631+danny4270@users.noreply.github.com>
2024-06-16 15:50:14 +0800

    Modify：更改delete之SQL語言寫法與PHP判斷式修改

--- SAS/SAS_SQLDelete.php
+++ SAS/SAS_SQLDelete.php
@@ -9,22 +9,50 @@ if(isset( $_POST['uid'])){
 
     switch ($identity){
         case 'T':
-            $sql1 = 'DELETE FROM teacher_profile WHERE uid = "'.$uid.'"';
-            $sql2 = 'DELETE FROM user_profile WHERE uid = "'.$uid.'"';
+            $sql0 = 'SELECT COUNT(*) FROM teacher_profile WHERE t_uid = :uid';
+            $sql1 = 'DELETE FROM teacher_profile WHERE t_uid = :uid';
+            $sql2 = 'DELETE FROM user_profile WHERE uid = :uid';
             break;
 
         case 'L':
-            $sql1 = 'DELETE FROM landlord WHERE uid = "'.$uid.'"';
-            $sql2 = 'DELETE FROM user_profile WHERE uid = "'.$uid.'"';
+            $sql0 = 'SELECT COUNT(*) FROM landlord WHERE uid = :uid';
+            $sql1 = 'DELETE FROM landlord WHERE uid = :uid';
+            $sql2 = 'DELETE FROM user_profile WHERE uid = :uid';
             break;
 
         case 'S':
-            $sql1 = 'DELETE FROM basicinfo WHERE uid = "'.$uid.'"';
-            $sql2 = 'DELETE FROM user_profile WHERE uid = "'.$uid.'"';
+            $sql0 = 'SELECT COUNT(*) FROM basicinfo WHERE uid = :uid';
+            $sql1 = 'DELETE FROM basicinfo WHERE uid = :uid';
+            $sql2 = 'DELETE FROM user_profile WHERE uid = :uid';
             break;
     }
-    $stmt1 = $conn -> prepare($sql1);
-    $stmt2 = $conn -> prepare($sql2);
+    $stmt0 = $conn->prepare($sql0);
+    $stmt2 = $conn->prepare($sql2);
+    $stmt0->bindParam(':uid', $uid);
+    $stmt2->bindParam(':uid', $uid);
+
+    $stmt0->execute();
+    $count = $stmt0->fetchColumn();
+    if($count !== 0){
+        $stmt1 = $conn->prepare($sql1);
+        $stmt1->bindParam(':uid', $uid);
+        if($stmt1->execute()){
+            if($stmt2->execute()){
+                echo '<script>deletemsg()</script>';
+            }else{
+                echo '<script>warning5()</script>';
+            }
+        }else{
+            echo '<script>warning5()</script>';
+        }
+    }else{
+        if($stmt2->execute()){
+            echo '<script>deletemsg()</script>';
+        }else{
+            echo '<script>warning5()</script>';
+        }
+    }
+/*
     try{
         $stmt1 = $conn -> exec($sql1);
         try{
@@ -36,6 +64,7 @@ if(isset( $_POST['uid'])){
     }catch (PDOException $e){
         echo '<script>warning6()</script>';
     }
+*/
 }else{
 
 }
